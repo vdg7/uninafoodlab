5ccb9314b543e0714db8d361cdbee00364ce2853
Vincenzo Del Gaudio <vdg@Mac.Home>
2026-01-26 12:11:11 +0100

    UPDATE UiUtil

--- src/main/java/it/uninafoodlab/gui/Util.java
+++ src/main/java/it/uninafoodlab/gui/Util.java
@@ -6,4 +6,6 @@ public class Util {
 	public static final Color BACKGROUND_GREY = new Color(230, 230, 240);
 	
 	public static final Color UNINA_BLUE = new Color(0, 46, 114);	
+	
+	public static final Color UNINA_GREY = new Color(110, 135, 175);
 }
